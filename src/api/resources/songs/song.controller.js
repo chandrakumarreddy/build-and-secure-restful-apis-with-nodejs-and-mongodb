fb6a566cca9e91a5ecac772501ed422be3108005
chandra655 <chandra.penugonda@unacademy.com>
2020-10-07 21:20:37 +0530

    remove console.log

--- src/api/resources/songs/song.controller.js
+++ src/api/resources/songs/song.controller.js
@@ -1,6 +1,5 @@
 export default class SongsController {
     static getSongs(req, res) {
-        console.log('object');
         res.send('TO DO LIST ALL SONGS');
     }
     static postSongs(req, res) {
